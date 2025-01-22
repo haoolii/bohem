9ba81db3bc66c4e592bdd3df2cf76e86bbcec9de
unnhao <unnhao@gmail.com>
2025-01-22 08:21:39 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://bohem-api.vercel.app/api/:path*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
